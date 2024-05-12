98658882a714567e20b93b2d91dbe880783d4eeb
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-05-12 17:30:03 +0200

    flight tracker updload

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -65,7 +65,7 @@ const Projects = ({ darkMode }) => {
             }`}
             onClick={() => filterProjects("frontend")}
           >
-            Frontend
+            Front-end
           </button>
           <button
             className={`project-border     px-6 py-2 dark:border-[#55e5a4] border-[#26313F] ${
@@ -98,11 +98,11 @@ const Projects = ({ darkMode }) => {
           ))}
         </div>
 
-        <div className=" justify-center md:pt-0 pt-8 pb-3 md:pb-0 items-center flex flex-col">
+        {/* <div className=" justify-center md:pt-0 pt-8 pb-3 md:pb-0 items-center flex flex-col">
           <button className="mr-4 project-border  md:text-xl px-3 md:px-6 py-2 dark:border-[#55e5a4] border-[#26313F]  hover:border-none hover:bg-[#00142D] dark:hover:bg-[#00A359]  dark:bg-[#55e5a4] bg-[#151C25] dark:text-[#000000] text-[#FFFFFF]">
             View all Projects
           </button>
-        </div>
+        </div> */}
       </div>
       <VideoModal isOpen={modalOpen} videourl={selectedVideoUrl} close={closeModal} />
     </section>
