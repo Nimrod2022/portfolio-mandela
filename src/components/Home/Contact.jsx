6a8f3d9c161a464416537a79bf0fb283d0e4a3b6
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-05-14 17:24:06 +0200

    mobile-contact form

--- src/components/Home/Contact.jsx
+++ src/components/Home/Contact.jsx
@@ -113,7 +113,7 @@ const Contact = ({ darkMode }) => {
           <h1 className=" text-[#26313F] dark:text-white text-2xl md:text-3xl font-extrabold">
             Get in Touch
           </h1>
-          <div className="w-full md:flex rounded-lg overflow-hidden   md:w-auto my-10  md:px-0 px-3 ">
+          <div className="w-full md:flex rounded-lg  md:overflow-hidden   md:w-auto my-10  md:px-0 px-3 ">
             <div className="bg-[url('/assets/contact-image.png')] hidden md:block form-height contact-form-image">
               <h1 className="text-white  justify-center items-center text-4xl px-20 pt-44">
                 Let’s discuss
@@ -131,57 +131,56 @@ const Contact = ({ darkMode }) => {
               </h1>
             </div>
 
-            
-              <div className=" rounded-lg md:rounded-none form-height  bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
-                <p className="text-lg">I am interested in...</p>
-                <form
-                  action="#"
-                  className="md:space-y-4 space-y-3 z-50 "
-                  onSubmit={sendEmail}
-                >
-                  <div className="flex justify-between gap-4 md:gap-10 pt-5">
-                    <div className="flex-1">
-                      <label
-                        htmlFor="Frontend"
-                        className="block w-full cursor-pointer rounded-lg border  border-gray-200 p-2 text-[#B4A7BC] hover:border-gray-400 has-[:checked]:border-none has-[:checked]:bg-[#26313F]  dark:has-[:checked]:bg-[#55E5A4] has-[:checked]:text-white dark:has-[:checked]:text-[#000000]"
-                        tabIndex="0"
-                      >
-                        <input
-                          className="sr-only h-6 w-6"
-                          id="Frontend"
-                          type="radio"
-                          tabIndex="-1"
-                          name="option"
-                          value="Frontend"
-                          ref={frontendRadioButtonRef}
-                          onChange={(e) => setInterest(e.target.value)}
-                        />
-                        <p className="text-md font-extrabold text-center">
-                          Frontend
-                        </p>
-                      </label>
-                    </div>
-
-                    <div className="flex-1">
-                      <label
-                        htmlFor="geoinformatics"
-                        className="block w-full cursor-pointer rounded-lg border border-gray-200 p-2 text-[#B4A7BC] hover:border-gray-400 has-[:checked]:border-none has-[:checked]:bg-[#26313F]  dark:has-[:checked]:bg-[#55E5A4] has-[:checked]:text-white dark:has-[:checked]:text-[#000000]"
-                      >
-                        <input
-                          className="sr-only h-6 w-6"
-                          id="geoinformatics"
-                          type="radio"
-                          tabIndex="-1"
-                          name="option"
-                          value="Geoinformatics"
-                          onChange={(e) => setInterest(e.target.value)}
-                        />
-                        <p className="text-md text-center "> Geoinformatics </p>
-                      </label>
-                    </div>
+            <div className=" rounded-lg md:rounded-none form-height  bg-white p-8 shadow-lg lg:col-span-3 lg:p-12">
+              <p className="text-lg">I am interested in...</p>
+              <form
+                action="#"
+                className="md:space-y-4 space-y-2 z-50 "
+                onSubmit={sendEmail}
+              >
+                <div className="flex justify-between gap-4 md:gap-10 pt-5">
+                  <div className="flex-1">
+                    <label
+                      htmlFor="Frontend"
+                      className="block w-full cursor-pointer rounded-lg border  border-gray-200 p-2 text-[#B4A7BC] hover:border-gray-400 has-[:checked]:border-none has-[:checked]:bg-[#26313F]  dark:has-[:checked]:bg-[#55E5A4] has-[:checked]:text-white dark:has-[:checked]:text-[#000000]"
+                      tabIndex="0"
+                    >
+                      <input
+                        className="sr-only h-6 w-6"
+                        id="Frontend"
+                        type="radio"
+                        tabIndex="-1"
+                        name="option"
+                        value="Frontend"
+                        ref={frontendRadioButtonRef}
+                        onChange={(e) => setInterest(e.target.value)}
+                      />
+                      <p className="text-md font-extrabold text-center">
+                        Frontend
+                      </p>
+                    </label>
                   </div>
 
-                  {/* <div className="hidden  flex-1 pt-5">
+                  <div className="flex-1">
+                    <label
+                      htmlFor="geoinformatics"
+                      className="block w-full cursor-pointer rounded-lg border border-gray-200 p-2 text-[#B4A7BC] hover:border-gray-400 has-[:checked]:border-none has-[:checked]:bg-[#26313F]  dark:has-[:checked]:bg-[#55E5A4] has-[:checked]:text-white dark:has-[:checked]:text-[#000000]"
+                    >
+                      <input
+                        className="sr-only h-6 w-6"
+                        id="geoinformatics"
+                        type="radio"
+                        tabIndex="-1"
+                        name="option"
+                        value="Geoinformatics"
+                        onChange={(e) => setInterest(e.target.value)}
+                      />
+                      <p className="text-md text-center "> Geoinformatics </p>
+                    </label>
+                  </div>
+                </div>
+
+                {/* <div className="hidden  flex-1 pt-5">
                     <label
                       htmlFor="other"
                       className="block cursor-pointer rounded-lg border border-gray-200 p-2 w-16 text-[#B4A7BC] hover:border-gray-400 has-[:checked]:border-none has-[:checked]:bg-[#26313F]  dark:has-[:checked]:bg-[#55E5A4] has-[:checked]:text-white dark:has-[:checked]:text-[#000000]"
@@ -198,88 +197,87 @@ const Contact = ({ darkMode }) => {
                     </label>
                   </div> */}
 
-                  <div />
+                <div />
 
+                <div>
+                  <label className="sr-only" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                    className="w-full rounded-lg border border-gray-200 p-3 text-sm focus:outline-none dark:focus:border-[#55E5A4] focus:border-[#26313F]"
+                    type="text"
+                    id="name"
+                    required
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="John Doe"
+                  />
+                </div>
+
+                <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
                   <div>
-                    <label className="sr-only" htmlFor="name">
-                      Name
+                    <label className="sr-only" htmlFor="email">
+                      Email
                     </label>
                     <input
                       className="w-full rounded-lg border border-gray-200 p-3 text-sm focus:outline-none dark:focus:border-[#55E5A4] focus:border-[#26313F]"
-                      type="text"
-                      id="name"
+                      type="email"
+                      id="email"
                       required
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                      placeholder="John Doe"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="johndoe@gmail.com"
                     />
                   </div>
 
-                  <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
-                    <div>
-                      <label className="sr-only" htmlFor="email">
-                        Email
-                      </label>
-                      <input
-                        className="w-full rounded-lg border border-gray-200 p-3 text-sm focus:outline-none dark:focus:border-[#55E5A4] focus:border-[#26313F]"
-                        type="email"
-                        id="email"
-                        required
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        placeholder="johndoe@gmail.com"
-                      />
-                    </div>
-
-                    <div>
-                      <label className="sr-only" htmlFor="phone">
-                        Phone
-                      </label>
-                      <input
-                        className="w-full rounded-lg border border-gray-200 p-3 text-sm focus:outline-none dark:focus:border-[#55E5A4] focus:border-[#26313F]"
-                        placeholder="+43 600 000 000"
-                        type="text"
-                        id="phone"
-                        value={phone}
-                        onChange={(e) => setPhone(e.target.value)}
-                      />
-                    </div>
-                  </div>
-
                   <div>
-                    <label className="sr-only " htmlFor="message">
-                      Message
+                    <label className="sr-only" htmlFor="phone">
+                      Phone
                     </label>
-
-                    <textarea
-                      className="w-full rounded-lg border border-gray-200 p-3 text-sm focus:outline-none dark:focus:border-[#55E5A4] focus:border-[#26313F] "
-                      placeholder="Message"
-                      rows="4"
-                      id="message"
-                      required
-                      value={message}
-                      onChange={(e) => setMessage(e.target.value)}
-                    ></textarea>
+                    <input
+                      className="w-full rounded-lg border border-gray-200 p-3 text-sm focus:outline-none dark:focus:border-[#55E5A4] focus:border-[#26313F]"
+                      placeholder="+43 600 000 000"
+                      type="text"
+                      id="phone"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                    />
                   </div>
+                </div>
 
-                  <div className=" md:pt-0 flex flex-col items-center justify-center ">
-                    <button
-                      type="submit"
-                      className={`inline-block w-auto text-md rounded-lg bg-[#26313F] dark:bg-[#55E5A4] px-3 py-2 md:px-5 md:py-3 font-bold text-white dark:text-[#151C25] sm:w-auto `}
-                    >
-                      <span className="flex items-center gap-4">
-                        <img
-                          src={darkMode ? darkMessageIcon : lightMessageIcon}
-                          alt="message-icon"
-                          className="size-5"
-                        />
-                        <p>Send message</p>
-                      </span>
-                    </button>
-                  </div>
-                </form>
-              </div>
-            
+                <div>
+                  <label className="sr-only " htmlFor="message">
+                    Message
+                  </label>
+
+                  <textarea
+                    className="w-full rounded-lg border border-gray-200 p-3 text-sm focus:outline-none dark:focus:border-[#55E5A4] focus:border-[#26313F] "
+                    placeholder="Message"
+                    rows="4"
+                    id="message"
+                    required
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                  ></textarea>
+                </div>
+
+                <div className=" md:pt-0 flex flex-col items-center justify-center ">
+                  <button
+                    type="submit"
+                    className={`inline-block w-auto text-md rounded-lg bg-[#26313F] dark:bg-[#55E5A4] px-3 py-2 md:px-5 md:py-3 font-bold text-white dark:text-[#151C25] sm:w-auto `}
+                  >
+                    <span className="flex items-center gap-4">
+                      <img
+                        src={darkMode ? darkMessageIcon : lightMessageIcon}
+                        alt="message-icon"
+                        className="size-5"
+                      />
+                      <p>Send message</p>
+                    </span>
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </section>
