0ce772f224237217eb9f04a0f13d08aa5982c88b
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-05-14 10:27:21 +0200

    about width

--- src/components/Home/About.jsx
+++ src/components/Home/About.jsx
@@ -5,28 +5,30 @@ const About = ({ darkMode }) => {
   return (
     <>
       <div id="/about" className={darkMode ? "dark" : ""}>
-        <div className=" dark:bg-[#151C25] bg-[#F0F0F4] md:gap-16 px-8 md:px-80 satoshi md:flex md:pt-40 pt-10  justify-center ">
+        <div className=" dark:bg-[#151C25] bg-[#F0F0F4] aboutBox md:gap-16 px-8 md:px-48  satoshi md:flex md:pt-40 pt-10  justify-center ">
           {/* Image */}
 
           <img
             src={darkMode ? darkModeProfileAbout : lightModeProfileAbout}
             alt="about-profile"
-            className="h-80 hidden md:block "
+            className="image hidden md:block "
           />
 
           {/* Text */}
-          <div className="">
-            <h1 className="text-3xl pt-10 md:pt-0 text-center md:text-start font-extrabold text-[#000000] dark:text-white">
-              About Me
-            </h1>
-            <div className="flex gap-2 pt-2 md:justify-start  justify-center items-center">
-              <hr className="about-line w-[5%] ml-6 border-[#26313F] dark:border-[#55E5A4]" />
-              <h3 className="text-[#26313F] font-bold dark:text-[#55E5A4] text-sm md:text-start text-center first-line:font-semibold">
-                Who am I?
-              </h3>
+          <div className="md:image">
+            <div className="aboutTitle">
+              <h1 className="text-3xl pt-10 md:pt-0 text-center md:text-start font-extrabold text-[#000000] dark:text-white">
+                About Me
+              </h1>
+              <div className="flex gap-2 pt-2 md:justify-start  justify-center items-center">
+                <hr className="about-line w-[5%] ml-6 border-[#26313F] dark:border-[#55E5A4]" />
+                <h3 className="text-[#26313F] font-bold dark:text-[#55E5A4] text-sm md:text-start text-center first-line:font-semibold">
+                  Who am I?
+                </h3>
+              </div>
             </div>
 
-            <p className="md:w-[95%]  text-lg pt-5 text-[#686565] dark:text-[#A9A9A9] md:text-start text-center">
+            <p className="md:w-full md:aboutText text-lg pt-5 text-[#686565] dark:text-[#A9A9A9] md:text-start text-center">
               I am currently a master’s student in Applied Geoinformatics at the
               University of Salzburg, Austria. I have a keen interest in
               front-end and GIS development. My goal is to use my expertise in
@@ -35,10 +37,8 @@ const About = ({ darkMode }) => {
               skills to engineer innovative, customizable, and sustainable
               software solutions.
             </p>
-            <h3 className="text-[#000000] font-extrabold dark:text-[#55E5A4] pt-8 text-xl md:text-start text-center">
-              Technologies
-            </h3>
-            <div className="flex pt-5 gap-4  md:gap-6">
+
+            <div className="flex technologiesmobile md:technologies">
               <div>
                 <img
                   src="/assets/react.svg"
@@ -102,7 +102,7 @@ const About = ({ darkMode }) => {
                 </h3>
               </div>
             </div>
-            <div className="pt-8 pb-20 flex justify-center md:justify-start">
+            <div className="resumemobile items-center justify-center md:justify-start flex md:resume md:pt-10 ">
               <button
                 type="button"
                 className="border-2 border-[#26313F] font-bold dark:hover:bg-[#02D375] hover:border-none dark:hover:text-black hover:text-white hover:bg-[#00142D] dark:border-[#55E5A4] text-md  w-48 rounded-md py-2 px-2 text-[#26313F] dark:text-[#55E5A4]"
