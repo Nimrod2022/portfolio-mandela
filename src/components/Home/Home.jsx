06c84f827dd3fbe2dae3b564993222ada664292c
Nimrod2022 <nimrodmandelakibet@gmail.com>
2024-07-07 11:42:37 +0200

    pt home-prodile

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -49,7 +49,7 @@ const Home = ({ darkMode, scrollToContact }) => {
   return (
     <>
       <div id="home" className={darkMode ? "dark" : ""}>
-        <div className="dark:bg-[#151C25] bg-[#F0F0F4] min-h-screen satoshi pt-20 md:pt-0 flex flex-col md:justify-center">
+        <div className="dark:bg-[#151C25] bg-[#F0F0F4] min-h-screen satoshi pt-20 md:pt-24 lg:pt-40 flex flex-col ">
           <div className="px-8 text-white md:flex md:gap-5 md:justify-center">
             <div className="items-center md:w-[45%]">
               <div className="flex items-center justify-center">
@@ -62,10 +62,10 @@ const Home = ({ darkMode, scrollToContact }) => {
                   />
                 </div>
               </div>
-              <p className="text-3xl md:text-5xl pt-3 text-[#26313F] dark:text-white text-center">
+              <p className="text-3xl md:text-5xl pt-3 md:pt-5 text-[#26313F] dark:text-white text-center">
                 Nimrod Kibet
               </p>
-              <h1 className="text-[#55E5A4] pt-2 font-extrabold text-2xl md:text-3xl text-center">
+              <h1 className="text-[#55E5A4] pt-2 md:pt-5 font-extrabold text-2xl md:text-3xl text-center">
                 Front-end Developer
               </h1>
               <p className="text-xl text-[#686565] dark:text-[#A9A9A9] pt-3 text-center">
